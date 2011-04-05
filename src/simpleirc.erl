e17f87134faab97100e50c198f7280ed01e13cca
solarus <solarus@tunna.org>
2011-04-05 07:42:10 +0200

    parse_message code

--- src/simpleirc.erl
+++ src/simpleirc.erl
@@ -13,13 +13,38 @@ connect (Host, Port, Name, Pass, Options) ->
 ping (Handle, Target, Timeout) ->
     Ref = make_ref(),
     Pid = self(),
+    T1 = now(),
     spawn (fun () ->
 		   Pid ! {Ref, catch gen_server:call(Handle, {ping, Target}, Timeout)}
 	   end),
     receive
-	{Ref, Result} ->
-	    Result
+	{Ref, {'EXIT', {Reason, _}}} ->
+	    {error, Reason};
+	{Ref, ok} ->
+	    T2 = now(),
+	    timer:now_diff(T1, T2)
     end.
-	
-	
-    
+
+parse_message (Message) ->
+    case binary_to_list(Message) of
+	[ $: | Tail ] -> %% got : we have a prefix
+	    [ Prefix, Command | Rest ] = string:tokens(Tail, " "),
+	    case parse_prefix(Prefix) of
+		{Nick, User, Host} -> From = {user, Nick, User, Host};
+		Host               -> From = {host, Host}
+	    end,
+	    Params = lists:takewhile(fun (X) -> not (params_end(X)) end, Rest),
+	    {From, Command, Params};
+	M ->
+	    M
+    end.
+
+parse_prefix (Str) ->
+    Str.
+
+params_end (":") ->
+    true;
+params_end (":\r\n") ->
+    true;
+params_end (_) ->
+    false.
