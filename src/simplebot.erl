aef28ae6b465a4f3bbaeee4307c0d92b266231fa
solarus <solarus@tunna.org>
2011-09-27 22:51:19 +0200

    Add simpleirc behaviour.

--- src/simplebot.erl
+++ src/simplebot.erl
@@ -2,6 +2,8 @@
 -export([test/0, privmsg/2, notice/2, invite/2, join/2]).
 -include("simpirc_common.hrl").
 
+-behaviour(simpleirc).
+
 test () ->
     {ok, _Handle} = simpleirc:start(?MODULE, {local, simpirc}, "127.0.0.1", 7000, "bott", nil, [ssl]).
 
