fd348cb9f46c15d4f3b965be46f13e29971082f0
solarus <solarus@tunna.org>
2011-10-08 22:23:39 +0200

    Thread safe logger

--- src/simpirc_logger.erl
+++ src/simpirc_logger.erl
@@ -2,14 +2,31 @@
 -export([log/2, log/3]).
 -include("simpirc_common.hrl").
 
+loop () ->
+    receive
+        {log, Level, Format, Args} ->
+            {H, M, S} = time(),
+            io:format("~2.10.0B:~2.10.0B:~2.10.0B | ~-8s| ", [H,M,S, level_to_string(Level)]),
+            io:format(Format, Args),
+            io:format("~n");
+        _ ->
+            ok
+    end,
+    loop().
+
 log (Level, Message) ->
     log(Level, "~s", [Message]).
 
 log (Level, Format, Args) when Level =< ?DEBUG ->
-    {H, M, S} = time(),
-    io:format("~2.10.0B:~2.10.0B:~2.10.0B | ~-8s| ", [H,M,S, level_to_string(Level)]),
-    io:format(Format, Args),
-    io:format("~n").
+    case whereis(?MODULE) of
+        undefined ->
+            spawn(fun () ->
+                          register(?MODULE, self()),
+                          loop()
+                  end);
+        Pid ->
+            Pid ! {log, Level, Format, Args}
+    end.
 
 level_to_string (?ERROR) ->
     " ERROR";
